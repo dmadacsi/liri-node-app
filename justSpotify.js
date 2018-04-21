01b2fb4c3b995a35c6e1718e0e1d56ba125c3b88
David <dmadacsi1@gmail.com>
2018-04-21 09:53:04 -0600

    questions woking

--- justSpotify.js
+++ justSpotify.js
@@ -1,45 +1,51 @@
 require("dotenv").config();
-var inquirer =require("inquirer");
+var inquirer = require("inquirer");
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js")
-var request = require( "request");
+var request = require("request");
 
 var spotify = new Spotify(keys.spotify);
 
 // var songName = process.argv.slice(2).join(' ');
 
 
- 
-  var spotObj={
-  spotify:function() {
-//put query name in name()
- var questions=[
-      
-      {
-        type:"input",
-        name:"song",
-        message:"What song are you looking for?",
-        
-      },
-      
-    ];
-    
-  inquirer.prompt(questions).then(function (answers){
-    console.log(JSON.stringify(answers, null, ' '));
-    console.log(answers.song);
-    var searchMe = answers.song;
-  });
-  
-  function spotSearch(){
-  spotify.search({ type: 'track', query: `${searchMe}`, limit:[1] }, function(err, data,) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  
-    console.log(data.tracks.items) 
-  
-  });
-  }
 
-}} 
+var spotObj = {
+  spotify: function() {
+    //put query name in name()
+    var questions = [
+
+      {
+        type: "input",
+        name: "song",
+        message: "What song are you looking for?",
+
+      },
+
+    ];
+
+    inquirer.prompt(questions).then(function (answers) {
+      // console.log(JSON.stringify(answers, null, ' '));
+      // console.log(answers.song);
+
+      spotSearch(answers.song)
+    });
+
+    function spotSearch(searchMe) {
+      spotify.search({
+        type: 'track',
+        query: `${searchMe}`,
+        limit: [1]  
+      }, function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data.tracks.items)
+
+      });
+    }
+
+  }
+}
 module.exports = spotObj;
--- liri.js
+++ liri.js
@@ -1,41 +1,63 @@
 require("dotenv").config();
-var Twitter =require("twitter")
-var Spotify =require("node-spotify-api");
+var Twitter = require("twitter")
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js")
-
+var inquirer = require("inquirer")
 var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
 
-var request = require( "request");
+var request = require("request");
 
 
 // var songName = process.argv.slice(3);
+// var movieName = process.argv[2]
+var pickFunc = { //had to remember to wrap these below in a object to call using module.export on questions page
+    omdb: function () {
+        var questions = [
 
-var pickFunc ={ //had to remember to wrap these below in a object to call using module.export on questions page
-omdb:function(movieName){
-    var movieName = process.argv[2]
-request(`http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-        console.log(`the movie info is:
+            {
+                type: "input",
+                name: "movie",
+                message: "What movie are you looking for?",
+
+            }
+
+        ];
+        inquirer.prompt(questions).then(function (answers) {
+            // console.log(JSON.stringify(answers, null, ' '));
+            console.log(answers.movie);
+            requestMovie(answers.movie);
+
+        });
+        // var movieName=answers.movie;
+
+        function requestMovie(movieName) {
+            request(`http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(`the movie info is:
         ${JSON.parse(body).Title} was made in ${JSON.parse(body).Year}
         Rating: ${JSON.parse(body).imdbRating} Country:${JSON.parse(body).Country}
         Language: ${JSON.parse(body).Language} Plot:${JSON.parse(body).Plot} 
         Actors: ${JSON.parse(body).Actors} Rotten Tomatoes  `)
-      }
-    });
-},
-twitter:function(){
-    client.get('statuses/user_timeline',{count:20}, function(error, tweets, response) {
-        if(error) throw error;
-           tweets.forEach(function(tweet) {
-               console.log(tweet.text);
-           }) 
-           }); 
+                    console.log(movieName, "!!!");
+                }
+            });
         }
+    },
+    twitter: function () {
+        client.get('statuses/user_timeline', {
+            count: 20
+        }, function (error, tweets, response) {
+            if (error) throw error;
+            tweets.forEach(function (tweet) {
+                console.log(tweet.text);
+            })
+        });
+    }
 }
 
-    module.exports=pickFunc;
-    // module.exports= {pickFunc};
+module.exports = pickFunc;
+// module.exports= {pickFunc};
 
-    //tomatoes ${JSON.parse(body).Ratings[1].Value}
+//tomatoes ${JSON.parse(body).Ratings[1].Value}
